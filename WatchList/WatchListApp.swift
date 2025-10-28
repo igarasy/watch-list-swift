3a80c917dbe8174d0384f66f7ee86ab13223112a
igarasy <igarasshi.lucass@gmail.com>
2025-10-28 08:55:41 -0300

    Initial Commit

---
+++ WatchList/WatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchListApp.swift
+//  WatchList
+//
+//  Created by lucas.igarashi on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
