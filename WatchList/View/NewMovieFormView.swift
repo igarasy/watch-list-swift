900c207c7f13941ead76dc56337d5b0995228c45
igarasy <igarasshi.lucass@gmail.com>
2025-10-30 09:58:30 -0300

    feat: add EmptyListView, ContentView and Genre model

---
+++ WatchList/View/NewMovieFormView.swift
@@ -0,0 +1,18 @@
+//
+//  NewMovieFormView.swift
+//  WatchList
+//
+//  Created by lucas.igarashi on 30/10/25.
+//
+
+import SwiftUI
+
+struct NewMovieFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewMovieFormView()
+}
